22a433156f86c3656a944e6b498ee06192fce170
Nafachri <nafa.create@gmail.com>
2022-12-08 12:45:40 +0700

    adding time and interactions plugins

--- src/components/FullCalendar/index.js
+++ src/components/FullCalendar/index.js
@@ -1,13 +1,27 @@
-import React from 'react'
-import FullCalendar from '@fullcalendar/react' // must go before plugins
-import dayGridPlugin from '@fullcalendar/daygrid' // a plugin!
+import React from "react";
+import FullCalendar from "@fullcalendar/react"; // must go before plugins
+import dayGridPlugin from "@fullcalendar/daygrid"; // a plugin!
+import timeGridPlugin from "@fullcalendar/timegrid";
+import interactionPlugin from "@fullcalendar/interaction";
 
-const FullCalendarComps =()=> {
-    return (
-      <FullCalendar
-        plugins={[ dayGridPlugin ]}
-        initialView="dayGridMonth"
-      />
-    )
-}
-export default FullCalendarComps
+const FullCalendarComps = () => {
+  return (
+    <FullCalendar
+      plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+      headerToolbar={{
+        left: "prev,next today",
+        center: "title",
+        right: "dayGridMonth,timeGridWeek,timeGridDay",
+      }}
+      initialView="dayGridMonth"
+      events={[
+        { title: "event 1", date: "2022-12-01" },
+        { title: "event 2", date: "2022-12-10" },
+      ]}
+      eventClick={() => console.log("event")}
+      editable={true}
+      selectable={true}
+    />
+  );
+};
+export default FullCalendarComps;
